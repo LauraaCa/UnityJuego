fa2bb2ac2cc978975fa3dc35e3d3882adb5472e8
LauraaCa <lauracastanedar962@gmail.com>
2025-11-22 13:35:20 -0500

    final hecho

--- Juego/Assets/Scripts/ZonaGanadora.cs
+++ Juego/Assets/Scripts/ZonaGanadora.cs
@@ -1,16 +1,81 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ZonaGanadora : MonoBehaviour
+public class ZonaGanadoraDebug : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("UI (opcional)")]
+    public GameObject pantallaGanaste;
+
+    [Header("Explosiones (opcional)")]
+    public GameObject explosionPrefab;
+    public float explosionRadius = 50f;
+    public float explosionForce = 800f;
+    public LayerMask explosionMask;
+    public LayerMask ignoreRoomLayer;
+
+    bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("[ZonaGanadora] OnTriggerEnter detectado con: " + other.name + " (tag: " + other.tag + ", layer: " + LayerMask.LayerToName(other.gameObject.layer) + ")");
+
+
+        if (!other.CompareTag("Player"))
+        {
+            Debug.Log("[ZonaGanadora] El objeto no tiene tag Player. Tag actual: " + other.tag);
+            return;
+        }
+
+        if (triggered)
+        {
+            Debug.Log("[ZonaGanadora] Ya se ha activado antes, ignorando.");
+            return;
+        }
+
+        triggered = true;
+
+        Debug.Log("¡¡ GANASTE !! - activando lógica de victoria");
+
+        if (pantallaGanaste != null)
+            pantallaGanaste.SetActive(true);
+        else
+            Debug.Log("[ZonaGanadora] pantallaGanaste es NULL");
+
+     
+        EliminarEnemigos();
     }
 
-    // Update is called once per frame
-    void Update()
+   
+
+    private void EliminarEnemigos()
     {
-        
+        GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+        Debug.Log("[ZonaGanadora] Enemigos encontrados con tag 'Enemy': " + enemigos.Length);
+
+        foreach (GameObject e in enemigos)
+        {
+            Debug.Log("[ZonaGanadora] Destruyendo enemigo: " + e.name);
+            Destroy(e);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+
+        Collider c = GetComponent<Collider>();
+        if (c != null)
+        {
+            Gizmos.color = new Color(0, 1, 0, 0.25f);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            if (c is BoxCollider box)
+            {
+                Gizmos.DrawCube(box.center, box.size);
+            }
+            else if (c is SphereCollider sphere)
+            {
+                Gizmos.DrawSphere(sphere.center, sphere.radius);
+            }
+        }
     }
 }
